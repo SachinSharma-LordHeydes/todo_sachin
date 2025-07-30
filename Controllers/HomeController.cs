6db3b7518a662fd00f4b299d500f25100411ce64
SachinSharma-LordHeydes <thereisamailforme@gmail.com>
2025-07-30 09:21:03 +0545

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using todosachin.Models;
+
+namespace todosachin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        public IActionResult Privacy()
+        {
+            ViewData["Title"] = "Learn Dot net.";
+            ViewData["Description"] = "Getting statarted with dotnet.";
+            ViewData["Status"] = "Done";
+
+            ViewBag.Task1 = "Start Projest";
+            ViewBag.Description1 = "Start TODO in web Dev";
+            ViewBag.Status1= "InProgress";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/testController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace todosachin.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
